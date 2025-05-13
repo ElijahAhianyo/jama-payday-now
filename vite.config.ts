77a0f7572c3883bae8a5b4915bda7bef2e8fdaab
Elijah <elijahahianyo@gmail.com>
2025-05-13 22:26:07 +0000

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from 'vite-plugin-pwa'
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
@@ -11,10 +12,34 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
+    VitePWA({
+      registerType: 'prompt',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Jama',
+        short_name: 'Jama',
+        description: 'A Digital banking app',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/apple-touch-icon.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/maskable-icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    }),
     mode === 'development' &&
     componentTagger(),
+    
   ].filter(Boolean),
-  base: '/jama-payday-now/',
+  // base: '/jama-payday-now/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
